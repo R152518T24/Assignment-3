7af80dbf7ef11086b45ae96c1870108966e670ce
Robert <chukwunyemrobert@gmail.com>
2025-06-18 03:05:15 +0100

    remove deprecated code

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const viewRoutes = require('./routes/views');
 app.use('/', viewRoutes);
 
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(MONGO_URI).then(() => {
   console.log('MongoDB connected');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
